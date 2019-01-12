9568f771206e88d2571e0a22a4923dcdd17363e3
Ryan Furrer <rfurrer@ualberta.ca>
2019-01-12 11:12:41 -0700

    Feature/Add reply connections to the graphs (#10)
    * Add reply connections to the graphs
    
    * networkx friendmap edge testing

---
+++ test/unit/test_friend_map.py
@@ -0,0 +1,168 @@
+from json import loads
+
+from undiscord.presentation.FriendMap import FriendMap
+
+MOCK_SERVER_DATA = loads("""
+{
+  "name": "ex",
+  "id": "00001",
+  "channels": [
+    {
+      "name": "ex channel",
+      "id": "00002",
+      "messages": [
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:27:09.000000",
+          "content": "msg 1",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 2",
+            "id": "2"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:26:11.000000",
+          "content": "msg 2",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:41.000000",
+          "content": "msg 3",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:40.000000",
+          "content": "msg 4",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:36.000000",
+          "content": "msg 5",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:31.000000",
+          "content": "msg 6",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:27.000000",
+          "content": "msg 7",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:17.000000",
+          "content": "msg 8",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 2",
+            "id": "2"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:15.000000",
+          "content": "msg 9",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:01.000000",
+          "content": "msg 10",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:25:00.000000",
+          "content": "msg 11",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 1",
+            "id": "1"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:24:58.000000",
+          "content": "msg 12",
+          "mentions": []
+        },
+        {
+          "author": {
+            "name": "user 2",
+            "id": "2"
+          },
+          "server": "ex server",
+          "channel": "ex channel",
+          "timestamp": "2018-11-11 11:24:45.000000",
+          "content": "msg 13",
+          "mentions": []
+        }
+      ]
+    }
+  ]
+}
+""")
+
+
+def test_graph_edges():
+    friends = FriendMap(MOCK_SERVER_DATA)
+    assert (list(friends.graph.edges()) == [('user 1', 'user 2'), ('user 2', 'user 1')])
+    assert friends.graph["user 1"]["user 2"]['weight'] == 2
+    assert friends.graph["user 2"]["user 1"]['weight'] == 3
--- undiscord/presentation/FriendMap.py
+++ undiscord/presentation/FriendMap.py
@@ -2,28 +2,37 @@ import networkx as nx
 from json import load
 import matplotlib.pyplot as plt
 
+from undiscord.reply_pry import get_possible_connections_from_server
+
 
 class FriendMap:
 
-    def __init__(self, data_file_name: str):
+    def __init__(self, server_data: dict):
         self.graph = nx.DiGraph()
-        with open(data_file_name, 'r') as file:
-            connections = load(file)
+        self.graph_title = server_data['name'] + " Network graph"
 
-        self.graph_title = connections['name'] + " Network graph"
-
-        for channel in connections["channels"]:
+        for channel in server_data["channels"]:
             for message in channel['messages']:
-                self.add_connections(message)
-        pass
+                self.add_connections_from_mentions(message)
+        self.add_reply_connections(server_data)
 
-    def add_connections(self, message: dict):
+    def add_reply_connections(self, data):
+        for message, reply in get_possible_connections_from_server(data):
+            if self.graph.has_edge(message["author"]["name"], reply["author"]["name"]):
+                self.graph[message["author"]["name"]][reply["author"]["name"]]['weight'] += 1
+            else:
+                self.graph.add_edge(message["author"]["name"], reply["author"]["name"], weight=1)
+
+    def add_connections_from_mentions(self, message: dict):
         author = message['author']['name']
         if not message['mentions']:
             self.graph.add_node(author)
 
         for mention in message['mentions']:
-            self.graph.add_edge(author, mention['name'])
+            if self.graph.has_edge(author, mention['name']):
+                self.graph[author][mention['name']]['weight'] += 1
+            else:
+                self.graph.add_edge(author, mention['name'], weight=1)
         pass
 
     def plot(self, filename: str):
@@ -31,9 +40,10 @@ class FriendMap:
         nx.draw_networkx(self.graph)
         plt.axis('off')
         plt.title(self.graph_title)
-        plt.show()
-        plt.savefig(filename, format="PNG")
+        plt.show(block=False)
+        plt.savefig(filename, format="svg")
 
 
-map = FriendMap("data.json")
-map.plot("test.png")
+if __name__ == "__main__":
+    map = FriendMap(load("discord-ding-ding_messages.json"))
+    map.plot("test.svg")
